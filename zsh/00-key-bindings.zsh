ffcfde0fac7e6a616ecd9333dced288058ed28e0
Koen Hausmans <koen@hausmans.nl>
2017-01-16 14:47:04 +0100

    Reduced the key timeout of key mappings in zsh

--- zsh/00-key-bindings.zsh
+++ zsh/00-key-bindings.zsh
@@ -3,12 +3,18 @@
 bindkey -e
 
 # Use <C-x><C-e> for editing command line in vim
+# Also possible to use v in vi-mode
 autoload -U edit-command-line
 zle -N edit-command-line
 bindkey '^X^E' edit-command-line
 bindkey -M vicmd v edit-command-line
 
+# Enable going into vi-mode using <C-[>
 bindkey -M emacs '^[' vi-cmd-mode
 
+# Enable history expansion using the <space> key
 bindkey -M emacs ' ' magic-space
 bindkey -M viins ' ' magic-space
+
+# Reduce the key timeout in zsh to 100ms
+export KEYTIMEOUT=1
