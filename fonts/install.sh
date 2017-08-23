a9e8a5b1f97162e1ec6773a06e72343b514c7ba5
Koen Hausmans <koen@hausmans.nl>
2017-08-23 13:55:31 +0200

    Extracted common helper functions from several scripts and bundled them in a include file

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-if ! hash fc-cache 2>/dev/null ; then
-	echo "No font information commands found, therefore unnecessary to install additional fonts. Exiting..."
-	exit 0
-fi
+## DO NOT CHANGE BELOW
 
 # Get the current directory of this install script
-PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Include the helper functions
+source "$BASEDIR/../scripts/_helpers.sh"
+
+if ! hash fc-cache 2>/dev/null ; then
+    fail "No font information commands found, therefore unnecessary to install additional fonts."
+fi
 
 # Define the font directory
 FontDir="$HOME/.local/share/fonts"
@@ -17,8 +21,8 @@ FontConfigDirectory="$HOME/.config/fontconfig/conf.d/"
 mkdir -p $FontConfigDirectory
 
 # Copy all local files from this repository
-echo "Installing fonts located in dotfiles repository..."
-cp $PWD/*.otf $FontDir
+info "Installing fonts located in dotfiles repository..."
+cp $BASEDIR/*.otf $FontDir
 
 # Install some powerline fonts from github
 FontsToInstall=("DejaVuSansMono" "DroidSansMono" "Inconsolata-g" "Inconsolata" "InconsolataDz" "RobotoMono" "SourceCodePro")
@@ -27,12 +31,12 @@ PowerlineFontsRepository="https://github.com/powerline/fonts.git"
 TMPDIR=$(mktemp -d)
 pushd $TMPDIR > /dev/null
 
-echo "Downloading powerline fonts from github..."
+info "Downloading powerline fonts from github..."
 git clone --quiet $PowerlineFontsRepository .
 
 for Font in "${FontsToInstall[@]}"
 do
-    echo "Installing $Font..."
+    info "Installing $Font..."
 
     find "$Font" -name "*.[o,t]tf" -type f -print0 | xargs -0 -I % cp "%" "$FontDir/"
 done
--- fzf/install.sh
+++ fzf/install.sh
@@ -4,15 +4,26 @@ FZF_DIR="$HOME/.fzf"
 
 ## DO NOT CHANGE BELOW
 
-echo "Creating fzf folder..."
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "$BASEDIR/../scripts/_helpers.sh"
+
+info "Trying to install fzf"
+
+if [ -d $FZF_DIR ]
+then
+    fail "Cannot install fzf as the folder ($FZF_DIR) already exists"
+fi
 
 mkdir -p $FZF_DIR
 pushd $FZF_DIR > /dev/null
 
-echo "Checking out source..."
+info "Checking out source..."
 git clone --quiet https://github.com/junegunn/fzf .
 
-echo "Installing fzf binary..."
+info "Installing fzf binary..."
 ./install --bin 3>&1 2>&1 > /dev/null
+
+success "Finished installing fzf"
 
 popd > /dev/null
