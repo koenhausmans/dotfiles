ccaf1a44ca38380c9bc68ed215aee8a288e7252b
Koen Hausmans <koen@hausmans.nl>
2018-03-18 18:24:54 +0100

    Added new git prompt that works on more machines

---
+++ bash/git_prompt.bash
@@ -0,0 +1,41 @@
+# Slightly based on: https://github.com/jcgoble3/gitstuff/blob/master/gitprompt.sh
+
+function _git_branch() {
+    # -- Finds and outputs the current branch name by parsing the list of
+    #    all branches
+    # -- Current branch is identified by an asterisk at the beginning
+    # -- If not in a Git repository, error message goes to /dev/null and
+    #    no output is produced
+    git branch --no-color 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+}
+
+function _git_status() {
+    # Outputs a series of indicators based on the status of the
+    # working directory:
+    # + changes are staged and ready to commit
+    # ! unstaged changes are present
+    # ? untracked files are present
+    # S changes have been stashed
+    # P local commits need to be pushed to the remote
+    local status="$(git status --porcelain 2>/dev/null)"
+    local output=''
+    [[ -n $(egrep '^[MADRC]' <<<"$status") ]] && output="$output+"
+    [[ -n $(egrep '^.[MD]' <<<"$status") ]] && output="$output!"
+    [[ -n $(egrep '^\?\?' <<<"$status") ]] && output="$output?"
+    [[ -n $(git stash list) ]] && output="${output}S"
+    [[ -n $(git log --branches --not --remotes) ]] && output="${output}P"
+    [[ -n $output ]] && output="|$output"  # separate from branch name
+    echo $output
+}
+
+function _git_prompt() {
+    local _rst="\[\e[0m\]"
+    local _green="\[\e[32m\]"
+
+    local branch=$(_git_branch)
+    if [[ "$branch" != "" ]] ; then
+        local status=$(_git_status)
+        echo "$_rst($_green$branch$_rst$status) "
+    fi
+}
+
