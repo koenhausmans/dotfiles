1f929a957de8b062a6a049ef1600259af4e0cd46
Koen Hausmans <koen@hausmans.nl>
2018-06-25 01:04:19 +0200

    Changed z.sh installation to the bash/install.sh file instead of .bashrc

---
+++ bash/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+git_clone_or_pull () {
+    repo_path="$1"
+    repo_url="$2"
+    if [[ -d "$repo_path" ]] ; then
+        git -C "$repo_path" pull --quiet
+    else
+        git clone --quiet "$repo_url" "$repo_path"
+    fi
+}
+
+Z_HOME="$HOME/.local/share/z/"
+git_clone_or_pull "$Z_HOME" "git://github.com/rupa/z"
+
